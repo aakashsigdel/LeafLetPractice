22229c840f90d85553df1e9c7f23ba5e37fccf24
Aakash Sigdel <aakash.sigdel@gmail.com>
2015-06-29 15:08:06 +0545

    inital commit

---
+++ abc.js
@@ -0,0 +1,23 @@
+var aakash = 1;
+
+console.log("hello");
+
+function aakash() {
+	var aakash;
+	console.log("Aakash");
+}
+
+
+function lolo() {
+	conosle.log("there is");
+}
+
+function lolo() {
+	console.log("hello hello");
+}
+
+function kkash() {
+	console.log("hello there" + aak )
+}
+
+
